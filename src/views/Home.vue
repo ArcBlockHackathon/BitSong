ca51edba26d2c6b74a9303eded1f01d26aa8b966
Paper <paperhs901230@gmail.com>
2018-09-15 16:46:00 +0800

    format txs to tonelst

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
 
 <script>
 import Piano from '../components/Piano.vue';
-
+import svc from '../services/transactions.js'
 export default {
   name: 'Home',
   data(){
@@ -17,6 +17,19 @@ export default {
   components: {
     Piano,
   },
+  async mounted() {
+    console.log("hello")
+    this.nextToneList=[3,3,3,1,3,5, 1,5,3,6 ,7,7,6,5, 3,5,6,4,5]
+    for(var i= 200;i<250;i++){
+      let nextToneList = new Array()
+      for(var j=0;j<20;j++){
+        const result = await svc.getNumberTxs({page:i*20+j})
+        nextToneList.push((Math.ceil(result.reduce((total,num)=>(total+num),0)/100))%6+1)
+      }
+      console.log("result:",nextToneList)
+      this.toneList = nextToneList
+    }
+  }  
 }
 </script>
 
