8d2a1b90347cd07850098341b38d85c201729ce9
shawn <624835942@qq.com>
2018-09-15 17:28:47 +0800

    Fix home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,8 +16,8 @@ export default {
   name: 'Home',
   data(){
     return {
-      toneList: [1,3,2,4,5,6,7,3,2,4],
-      playedToneList: [],
+      toneList: [1,3,2,4,5,6,7,3,2,4,3,3,3,1,3,5, 1,5,3,6 ,7,7,6,5, 3,5,6,4,5],
+      playedToneList: Array(30).fill(0),
     }
   },
   components: {
@@ -31,11 +31,9 @@ export default {
     },
   },
   async mounted() {
-    this.toneList.push(3,3,3,1,3,5, 1,5,3,6 ,7,7,6,5, 3,5,6,4,5);
     for(var i= 4000;i < 5000;i++){
       const result = await transactionSvc.getStat({ page:i });
       this.toneList.push((Math.ceil(result.avg / 100)) % 6 + 1);
-      console.log(this.toneList);
     }
   }
 }
