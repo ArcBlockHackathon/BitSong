4c564ab3d18eb413d9a547f3d24db7da3e8525cf
Paper <paperhs901230@gmail.com>
2018-09-15 17:37:42 +0800

    change tone and rthme

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,18 +11,50 @@ export default {
   name: 'Home',
   data(){
     return {
-      toneList: [1,3,2,4,5,6,7,3,2,4],
+      toneList: [],
+      maxTxs:1000,
     }
   },
+  methods:{
+    getSpeed(speed,txs,lastTxs){
+    if(txs > lastTxs){
+      speed -= 50
+    }else {
+      speed += 50
+    }
+    if(speed < 200){
+      return 200
+    }
+    if(speed  > 500){
+      return 500
+    }
+    return speed
+  },
+    getTone(Txs){
+    if(Txs > this.maxTxs){
+      this.maxTxs = Txs
+      return 11
+    }else {
+      let rate = Txs/this.maxTxs
+      return Math.floor((rate-0.5)*20)
+    }
+    },
+  },
+  
   components: {
     Piano,
   },
   async mounted() {
     console.log("hello")
-    this.toneList.push(3,3,3,1,3,5, 1,5,3,6 ,7,7,6,5, 3,5,6,4,5)
+    //this.toneList.push(3,3,3,1,3,5, 1,5,3,6 ,7,7,6,5, 3,5,6,4,5)
+    let lastTxs = 1000
+    let lastSpeed = 400
     for(var i= 4000;i<5000;i++){
         const result = await svc.getNumberTxs({page:i})
-        this.toneList.push((Math.ceil(result.reduce((total,num)=>(total+num),0)/100))%6+1)
+        const Txs = Math.ceil((result.reduce((total,num)=>(total+num),0))/100)
+        lastSpeed = this.getSpeed(lastSpeed,Txs,lastTxs)
+        this.toneList.push({tone:this.getTone(Txs),speed:lastSpeed})
+        console.log("data:",{tone:this.getTone(Txs),speed:lastSpeed})
     }
   }  
 }
